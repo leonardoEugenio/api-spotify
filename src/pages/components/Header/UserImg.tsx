31577146c866466597c782ebd6676fa8c176473a
Leonardo Eugenio Mascarenhas Mantoni <51415831+leonardoEugenio@users.noreply.github.com>
2022-04-23 03:51:43 -0300

    get img user

--- src/pages/components/Header/UserImg.tsx
+++ src/pages/components/Header/UserImg.tsx
@@ -1,7 +1,12 @@
-export default function UserImg(img : any){
-    return(
-        <div>
-            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nicepng.com%2Fourpic%2Fu2q8i1a9e6i1o0o0_view-user-icon-png-user-circle-icon-png%2F&psig=AOvVaw1rVpv-Rtjtxr8F5bUNkFib&ust=1650773681912000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMijqqupqfcCFQAAAAAdAAAAABA3" alt="User" />
+import styles from './Header.module.scss'
+
+interface Props {
+    imgUrl : string
+}
+export default function UserImg(props: Props) {
+    return (
+        <div className={styles.bgImgUser}>
+            <img src={props.imgUrl} alt="ImageUser" />
         </div>
     )
 }
