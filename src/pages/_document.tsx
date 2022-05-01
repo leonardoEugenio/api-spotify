a98b8b81e529ff93ee38974580784b1823de9baf
Leonardo Eugenio Mascarenhas Mantoni <51415831+leonardoEugenio@users.noreply.github.com>
2022-05-01 17:24:05 -0300

    add font roboto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,9 @@ export default class MyDocument extends Document{
         return(
             <Html>
                 <Head>
-                {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
-                {/* <link rel="preconnect" href="https://fonts.gstatic.com"/> */}
-                {/* <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" /> */}
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
                 </Head>
                 <Main />
                 <NextScript />
