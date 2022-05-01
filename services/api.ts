47f713a3a25dd3c2d53b833e221f0bc1fc4b5160
Leonardo Eugenio Mascarenhas Mantoni <51415831+leonardoEugenio@users.noreply.github.com>
2022-05-01 17:19:40 -0300

    organizing folders

--- services/api.ts
+++
@@ -1,7 +0,0 @@
-import axius from 'axios'
-// import { useSession } from 'next-auth/react'
-
-export const apiSpotify = axius.create({
-    baseURL: 'https://api.spotify.com/v1/',
-    headers: {'Content-Type': 'application/json'}
-})
