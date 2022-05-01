ac43995ca39903a1dc747f1b95939857013f7926
Leonardo Eugenio Mascarenhas Mantoni <51415831+leonardoEugenio@users.noreply.github.com>
2022-05-01 17:24:49 -0300

    feeding index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,31 @@
 import styles from '../../styles/Home.module.scss'
-import {  FaSpotify } from 'react-icons/fa'
+import { FaSpotify } from 'react-icons/fa'
 import { CardTrack } from './components/CardTrak'
-import { useState } from 'react'
+import { useTracks } from './components/hooks/useTracks'
 
-interface Tracks{
-    items: [
-        duration_ms : number,
-        name: string, 
-        external_urls : {
-            spotify : string
-        }
-    ]
-}
 
-export function HandleTraks(params : any) {
-    setTraks(params)
-    console.log(traks);
-}
-const [traks, setTraks] = useState({})
 export default function Home() {
+    const { tracks } = useTracks()
+    console.log(tracks.length)
 
-
-    return(
+    return (
         <div className={styles.container}>
-            <h1>Tracks found</h1> 
-            <div className={styles.content}>
-                {/* <FaSpotify className={styles.logo}/>
-                <h1>Api Spotify</h1> */}
-                <CardTrack />
-                <CardTrack />
-                <CardTrack />
-                <CardTrack />
-                <CardTrack />
-                <CardTrack />
-                <CardTrack />
-                <CardTrack />
-            </div>
+            {
+                tracks.length != 0 ? <h1>Tracks found</h1> : <></>
+            }
+            <main className={styles.content}>
+                {
+                    tracks.length == 0 ? (
+                        <>
+                            <FaSpotify className={styles.logo} />
+                            <h1 className={styles.titleContent}>Api Spotify</h1>
+                        </>
+                    ) :
+                        tracks.map((track) => {
+                            return <CardTrack track={track} />
+                        })
+                }
+            </main>
         </div>
     )
 }
